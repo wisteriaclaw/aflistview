442a2dc78bc5f8f5330a7900a54875d902673ffc
wisteriaclaw <aspenboutilier@proton.me>
2025-04-10 13:33:31 -0700

    Add files via upload

---
+++ List View-2025-04-10.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         List View
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-10
+// @description  try to take over the world!
+// @author       You
+// @match        https://artfight.net/manage/bookmarks/folders/unorganized
+// @match        https://artfight.net/manage/bookmarks/folders/unorganized?page=1
+// @match        https://artfight.net/manage/bookmarks/folders/unorganized?page=2
+// @match        https://artfight.net/manage/bookmarks/folders/unorganized?page=3
+// @match        https://artfight.net/manage/bookmarks/folders/unorganized?page=4
+// @match        https://artfight.net/manage/bookmarks/folders/unorganized?page=5
+// @match        https://artfight.net/manage/bookmarks/folders/unorganized?page=6
+// @icon         https://artfight.net/manage/bookmarks/folders/unorganized
+// @grant        none
+// ==/UserScript==
+
+setTimeout(function() {
+    document.getElementById('viewListBtn').click();
+}, 1000)
+();
